a2e0c6315378ea2417f4595c8bad421c6cc5638e
looju <omofade2020@gmail.com>
2024-12-27 10:48:46 +0100

    native modules, keys

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 
 export default function RootLayout() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  if (!publishableKey) {
+    throw new Error("Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to your .env file");
+  }
   return <Stack />;
 }
