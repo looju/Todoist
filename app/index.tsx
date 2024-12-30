1439b24bb7547172915d8ac653a0c9711ad0b8c9
looju <omofade2020@gmail.com>
2024-12-30 10:33:05 +0100

    setup nativewind, metro, babel

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,38 @@
 import { Text, View } from "react-native";
-
+import { useOAuth } from "@clerk/clerk-expo";
+import { useCallback } from "react";
+import * as WebBrowser from "expo-web-browser";
 export default function Index() {
+  const { startOAuthFlow: googleOauth } = useOAuth({
+    strategy: "oauth_google",
+  });
+  const { startOAuthFlow: appleOauth } = useOAuth({ strategy: "oauth_apple" });
+
+  const handleAppleOAuth = useCallback(async () => {
+    try {
+      const { createdSessionId, setActive } = await appleOauth({
+        redirectUrl: Linking.createURL("/dashboard", { scheme: "myapp" }),
+      });
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      }
+    } catch (error) {}
+  }, []);
+
+  const handleGoogleOAuth = useCallback(async () => {
+    try {
+      const { createdSessionId, setActive } = await googleOauth({
+        redirectUrl: Linking.createURL("/dashboard", { scheme: "myapp" }),
+      });
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      }
+    } catch (error) {}
+  }, []);
+
+  const openLink = async () => {
+    await WebBrowser.openBrowserAsync("https://galaxies.dev");
+  };
   return (
     <View
       style={{
